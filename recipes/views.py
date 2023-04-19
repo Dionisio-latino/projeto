f8a2911226211e618774bbb6fc8c61beb97d1da9
Dionisio-latino <nicolasenfi@gmail.com>
2023-04-19 15:20:25 -0300

    Adicionados home's, settings e views modificado

--- recipes/views.py
+++ recipes/views.py
@@ -1,9 +1,9 @@
-# from django.shortcuts import render
+from django.shortcuts import render
 from django.http import HttpResponse
 
 
 def home(request):
-    return HttpResponse('HOME')
+    return render(request, 'global/home.html')
 
 
 def sobre(request):
